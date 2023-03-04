330531b966cb1390b3a56724dbe9ab6e07ed6842
ksunandr <ksun.andr@gmail.com>
2023-03-04 13:29:47 +0000

    Initial commit initialise spring boot app with dependencies

---
+++ src/main/java/com/ksun/mrsk/containerbooking/ContainerBookingApplication.java
@@ -0,0 +1,15 @@
+
+
+package com.ksun.mrsk.containerbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerBookingApplication.class, args);
+	}
+
+}
