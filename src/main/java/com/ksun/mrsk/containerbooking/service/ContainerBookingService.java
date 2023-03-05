fa3a41f4c2bda402a58c083fcbe23f92b3b33b89
ksunandr <ksun.andr@gmail.com>
2023-03-05 01:12:07 +0000

    before cassandra

--- src/main/java/com/ksun/mrsk/containerbooking/service/ContainerBookingService.java
+++ src/main/java/com/ksun/mrsk/containerbooking/service/ContainerBookingService.java
@@ -1,11 +1,11 @@
 package com.ksun.mrsk.containerbooking.service;
 
-import com.ksun.mrsk.containerbooking.model.Booking;
+//import com.ksun.mrsk.containerbooking.model.Booking;
 import com.ksun.mrsk.containerbooking.model.dto.AvailableCheck;
 import com.ksun.mrsk.containerbooking.model.dto.BookingRequest;
 import com.ksun.mrsk.containerbooking.model.dto.BookingResponse;
 import com.ksun.mrsk.containerbooking.model.dto.SpaceAvailabilityResponse;
-import com.ksun.mrsk.containerbooking.repository.BookingRepository;
+//import com.ksun.mrsk.containerbooking.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -13,18 +13,22 @@ import reactor.core.publisher.Mono;
 @Component
 public class ContainerBookingService {
 
-    private BookingRepository bookingRepository;
+   // private BookingRepository bookingRepository;
 
-    @Autowired
-    public ContainerBookingService(BookingRepository bookingRepository) {
-        this.bookingRepository = bookingRepository;
+
+    public ContainerBookingService() {
     }
+
+   // @Autowired
+  //  public ContainerBookingService(BookingRepository bookingRepository) {
+     //   this.bookingRepository = bookingRepository;
+   // }
 
     public Mono<BookingResponse> book(BookingRequest bookingRequest) {
 
-      Booking booking=  new Booking(null,1, "des", "e", 1, "tt39849yt");
+     // Booking booking=  new Booking(null,1, "des", "e", 1, "tt39849yt");
 
-        bookingRepository.save(booking);
+       // bookingRepository.save(booking);
 
         return Mono.just(new BookingResponse("00"));
 
