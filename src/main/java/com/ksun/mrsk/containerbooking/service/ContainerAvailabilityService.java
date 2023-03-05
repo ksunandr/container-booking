fabad366cbd89fc4b8556f98b85bb7865b128539
ksunandr <ksun.andr@gmail.com>
2023-03-05 01:27:12 +0000

    test for ContainerAvailabilityService

--- src/main/java/com/ksun/mrsk/containerbooking/service/ContainerAvailabilityService.java
+++ src/main/java/com/ksun/mrsk/containerbooking/service/ContainerAvailabilityService.java
@@ -3,12 +3,13 @@ package com.ksun.mrsk.containerbooking.service;
 import com.ksun.mrsk.containerbooking.model.dto.AvailableCheck;
 import com.ksun.mrsk.containerbooking.model.dto.BookingCheckRequest;
 
-//import jakarta.annotation.PostConstruct;
+
 import com.ksun.mrsk.containerbooking.model.dto.SpaceAvailabilityResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
 
 @Component
 public class ContainerAvailabilityService {
@@ -16,31 +17,21 @@ public class ContainerAvailabilityService {
     private final WebClient client;
 
     @Autowired
-    public ContainerAvailabilityService() {
-        this.client = WebClient.create("https://www.maersk.com");
+    public ContainerAvailabilityService(WebClient webClient) {
+        this.client = webClient;
     }
 
     public Mono<AvailableCheck> checkAvailable(BookingCheckRequest bookingRequest) {
+        return client.post()
+                .uri("/api/bookings/checkAvailable")
+                .retrieve()
+                .bodyToMono(SpaceAvailabilityResponse.class)
+                .map(e -> new AvailableCheck(e.getAvailableSpace() > 0));
 
 
-//        return client.post()
-//                .uri("/api/bookings/checkAvailable")
-//                .retrieve()
-//                .bodyToMono(SpaceAvailabilityResponse.class)
-//                .map(e -> new AvailableCheck(e.getAvailableSpace() > 0));
-//
-
-          return Mono.just(new SpaceAvailabilityResponse(0))
-                  .map(e -> new AvailableCheck(e.getAvailableSpace()>0));
+//          return Mono.just(new SpaceAvailabilityResponse(0))
+//                  .map(e -> new AvailableCheck(e.getAvailableSpace()>0));
 
 
     }
-
-
-//    @PostConstruct
-//    public void a() {
-//        checkAvailable(null);
-//    }
-
-
 }
--- src/test/java/com/ksun/mrsk/containerbooking/service/ContainerAvailabilityServiceTest.java
+++ src/test/java/com/ksun/mrsk/containerbooking/service/ContainerAvailabilityServiceTest.java
@@ -2,6 +2,7 @@ package com.ksun.mrsk.containerbooking.service;
 
 import com.ksun.mrsk.containerbooking.model.dto.AvailableCheck;
 import com.ksun.mrsk.containerbooking.model.dto.BookingCheckRequest;
+import com.ksun.mrsk.containerbooking.model.dto.BookingRequest;
 import com.ksun.mrsk.containerbooking.model.dto.ContainerType;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -9,14 +10,15 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.io.IOException;
 
 class ContainerAvailabilityServiceTest {
 
     public static MockWebServer mockBackEnd;
-
     public ContainerAvailabilityService sut;
 
     @BeforeAll
@@ -35,24 +37,36 @@ class ContainerAvailabilityServiceTest {
         String baseUrl = String.format("http://localhost:%s",
                 mockBackEnd.getPort());
 
+       sut = new ContainerAvailabilityService(WebClient.create(baseUrl));
+    }
 
-        sut = new ContainerAvailabilityService();
+    @Test
+    void testCheckAvailable_availableSpace0_false() {
 
+        mockBackEnd.enqueue(new MockResponse()
+                .setBody("{\"availableSpace\": 0}")
+                .addHeader("Content-Type", "application/json"));
+
+        Mono<AvailableCheck> response = sut.checkAvailable(new BookingCheckRequest());
+
+        StepVerifier.create(response)
+                .expectNextMatches(availableCheck -> !availableCheck.isAvailable())
+                .verifyComplete();
 
     }
 
     @Test
-    void checkAvailable() {
-        BookingCheckRequest bookingRequest = new BookingCheckRequest(1, ContainerType.DRY, "", "", 1);
+    void testCheckAvailable_availableSpace6_true() {
+
         mockBackEnd.enqueue(new MockResponse()
-                .setBody("1=1")
+                .setBody("{\"availableSpace\": 6}")
                 .addHeader("Content-Type", "application/json"));
 
-        Mono<AvailableCheck> result = sut.checkAvailable(bookingRequest);
+        Mono<AvailableCheck> response = sut.checkAvailable(new BookingCheckRequest());
 
-
-
-        System.out.println(result);
+        StepVerifier.create(response)
+                .expectNextMatches(AvailableCheck::isAvailable)
+                .verifyComplete();
 
     }
 }
